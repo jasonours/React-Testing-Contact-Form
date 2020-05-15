cc57ddfb578c5a5377ed673620964d669fca9a85
Jason Ours <jasonours@outlook.com>
2020-05-14 20:40:30 -0500

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,36 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("First Name", () => {
+  const { getByText } = render(<App />);
+  const firstName = getByText(/first name/i);
+  expect(firstName).toBeInTheDocument();
+});
+
+test("Last Name", () => {
+  const { getByText } = render(<App />);
+  const lastName = getByText(/last name/i);
+  expect(lastName).toBeInTheDocument();
+});
+
+test("E-Mail", () => {
+  const { getByText } = render(<App />);
+  const email = getByText(/email/i);
+  expect(email).toBeInTheDocument();
+});
+
+test("Message", () => {
+  const { getByText } = render(<App />);
+  const message = getByText(/message/i);
+  expect(message).toBeInTheDocument();
+});
+
+test("Submit Button", () => {
+  fireEvent.click(submitButton)
+
+})
